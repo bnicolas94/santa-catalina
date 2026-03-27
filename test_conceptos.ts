22fdf7aa8d9680c4b5f1e58fb9a16c1ff5135476
bnicolas94 <nico.leo.busto@gmail.com>
2026-03-27 13:53:05 -0300

    chore: cleanup test and scratch scripts

--- test_conceptos.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from './lib/prisma';
-async function main() {
-    const conceptos = await prisma.conceptoCaja.findMany();
-    console.log(JSON.stringify(conceptos, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
